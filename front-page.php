b4f60c1bdf04744f774c4c79f9193360ce6a32c8
krys0381 <krys0381@easv365.dk>
2023-04-23 22:15:17 +0200

    Working on Home

--- front-page.php
+++ front-page.php
@@ -29,4 +29,174 @@
 
 </div>
 
+<!-- HOME - GET TO KNOW US -->
+
+<div class="home-default-container container-fluid overflow-hidden">
+	<div class="container">
+	  	<div class="row d-flex justify-content-between">
+	  	  	<div class="home-default-image col-lg-6 col-sm-12 col-12 d-flex align-items-center">
+	  	  	  	<img src="<?php echo get_template_directory_uri(); ?>/images/get-to-know-us.png" alt="Owners of Det Lille Bageri">
+	  	  	</div>
+	  	  	<div class="home-default-text-container d-flex justify-content-center flex-column col-lg-5 col-sm-12 col-12">
+	  	  	  	<div class="home-default-text-content-container">
+	  	  	  	  	<h2>
+						We take pride in our <br>family-owned bakery.
+	  	  	  	  	</h2>
+	  	  	  	  	<p class="home-default-text-p">
+						At our bakery, we are more than just a business - we are a family. The bakery is owned and operated by a husband and wife duo who have poured their heart and soul into creating a warm and welcoming atmosphere for the customers.
+						<br></br>
+						With 25 years of experience in the baking industry, the husband has honed his skills in creating delicious cakes and bread that everyone loves. The wife works alongside her husband, bringing her expertise to the bakery. Together, they have owned the bakery for four years and take pride in being the only homey and affordable bakery in Henne Strand.
+	  	  	  	  	</p>
+	  	  	  	  	<div class="home-default-text-button col-lg-12 col-sm-12 col-12 d-flex justify-content-start align-item-center">
+						<a href="#"><button type="button" class="btn btn-link">GET TO KNOW US<i class="bi bi-arrow-right"></i></button></a>
+	  	  	  	  	</div>
+	  	  	  	</div>
+	  	  	</div>
+	  	</div>
+	</div>
+</div>
+
+<!-- HOME - SPECIAL OFFER -->
+
+<div class="offer-component container-fluid d-flex align-items-center justify-content-center overflow-hidden">  
+    <div class="container content-container d-flex align-items-center justify-content-center">
+        <div class="row d-flex flex-column align-items-center justify-content-start">
+            <div class="offer-content-container col-lg-10 col-sm-12 col-12 d-flex flex-column flex-wrap align-items-center justify-content-center">
+                <h2>Hop into Easter with our irresistible Easter cake.</h2>
+				<a href="#"><button type="button" class=" btn btn-primary btn-lg col-lg-12 col-sm-12 col-12">DISCOVER OUR DELIGHTS</button></a>
+            </div>
+        </div>
+    </div>
+    <div class="bg-image-container d-none d-xl-flex d-lg-flex d-md-flex align-item-center justify-self-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/special-offer.png" alt="Sliced Easter cake">
+    </div>
+</div>
+
+<!-- HOME - SHOP INTRODUCTION -->
+
+<div class="shop-introduction-component container-fluid">
+  
+	<div class="shop-introduction-row g-0 container">
+		<div class="row d-flex justify-content-between align-items-start">
+			<div class="col-lg-12 col-sm-12 col-12 d-flex flex-column align-items-center">
+				<div class="shop-introduction-container d-flex flex-column justify-content-between">
+					<div class="shop-introduction-text-container">
+						<h2>Welcome to our bakery’s <br>world of deliciousness!</h2>
+					</div>
+					<div class="shop-introduction-text-container d-flex flex-column align-items-center">
+						<p>Indulge in the authentic taste of Denmark with our freshly baked goods. We have everything from traditional Danish pastries to famous rye bread and sweet treats. We also offer a range of gluten-free options to cater to your dietary needs. Come and experience our heavenly baked goods today!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="shop-categories-row g-0 container">
+		<div class="row d-flex justify-content-between align-items-start">
+			<div class="card-group">
+    			<div class="col-lg-4 col-sm-12 col-12 d-flex justify-content-center my-2">
+
+					<div class="card shop-category">
+  						<img src="<?php echo get_template_directory_uri(); ?>/images/bread.png" class="card-img" alt="">
+  						<div class="card-img-overlay">
+						  	<div class="title-shadow"><h3 class="card-title">Bread</h3></div>
+  						</div>
+					</div>
+
+    			</div>
+    			<div class="col-lg-4 col-sm-12 col-12 d-flex justify-content-center my-2">
+
+					<div class="card shop-category">
+  						<img src="<?php echo get_template_directory_uri(); ?>/images/pastries.png" class="card-img" alt="">
+  						<div class="card-img-overlay">
+						  	<div class="title-shadow"><h3 class="card-title">Pastries</h3></div>
+  						</div>
+					</div>
+
+    			</div>
+    			<div class="col-lg-4 col-sm-12 col-12 d-flex justify-content-center my-2">
+
+					<div class="card shop-category">
+  						<img src="<?php echo get_template_directory_uri(); ?>/images/cakes.png" class="card-img" alt="">
+  						<div class="card-img-overlay">
+						  	<div class="title-shadow"><h3 class="card-title">Cakes</h3></div>
+  						</div>
+					</div>
+
+    			</div>
+    			<div class="col-lg-4 col-sm-12 col-12 d-flex justify-content-center my-2">
+
+					<div class="card shop-category">
+  						<img src="<?php echo get_template_directory_uri(); ?>/images/cookies.png" class="card-img" alt="">
+  						<div class="card-img-overlay">
+						  	<div class="title-shadow"><h3 class="card-title">Cookies</h3></div>
+  						</div>
+					</div>
+
+    			</div>
+    			<div class="col-lg-4 col-sm-12 col-12 d-flex justify-content-center my-2">
+
+					<div class="card shop-category">
+  						<img src="<?php echo get_template_directory_uri(); ?>/images/specialty-goods.png" class="card-img" alt="">
+  						<div class="card-img-overlay">
+						  	<div class="title-shadow"><h3 class="card-title">Specialty goods</h3></div>
+  						</div>
+					</div>
+
+    			</div>
+    			<div class="col-lg-4 col-sm-12 col-12 d-flex justify-content-center my-2">
+
+					<div class="card shop-category">
+  						<img src="<?php echo get_template_directory_uri(); ?>/images/gluten-free-goods.png" class="card-img" alt="">
+  						<div class="card-img-overlay">
+							<div class="title-shadow"><h3 class="card-title">Gluten-free goods</h3></div>
+  						</div>
+					</div>
+
+    			</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- HOME - RESERVE AND COLLECT -->
+
+<div class="home-default-container container-fluid overflow-hidden">
+	<div class="container">
+	  	<div class="row d-flex justify-content-between">
+	  	  	<div class="home-default-image col-lg-6 col-sm-12 col-12 d-flex align-items-center">
+	  	  	  	<img src="<?php echo get_template_directory_uri(); ?>/images/reserve-and-collect.png" alt="Owners of Det Lille Bageri">
+	  	  	</div>
+	  	  	<div class="home-default-text-container d-flex justify-content-center flex-column col-lg-5 col-sm-12 col-12">
+	  	  	  	<div class="home-default-text-content-container">
+	  	  	  	  	<h2>
+						Online reservation & in-store <br>pickup for fresh bakery delights.
+	  	  	  	  	</h2>
+	  	  	  	  	<p class="home-default-text-p">
+						Our online reservation and in-store pickup option make your shopping experience seamless and convenient. With a few effortless clicks, you can easily reserve your bakery delights from the comfort of your home or on the go. We ensure a smooth and efficient reservation process, allowing you to select your desired items, choose what suits your convenience, and have them prepared for pickup at our store.	  	  	  	  	</p>
+	  	  	  	  	<div class="home-default-text-button col-lg-12 col-sm-12 col-12 d-flex justify-content-start align-item-center">
+						<a href="#"><button type="button" class="btn btn-link">RESERVE AND COLLECT<i class="bi bi-arrow-right"></i></button></a>
+	  	  	  	  	</div>
+	  	  	  	</div>
+	  	  	</div>
+	  	</div>
+	</div>
+</div>
+
+<!-- HOME - CUSTOM CAKES -->
+
+<div class="offer-component container-fluid d-flex align-items-center justify-content-center overflow-hidden">  
+    <div class="container content-container d-flex align-items-center justify-content-center">
+        <div class="row d-flex flex-column align-items-center justify-content-start">
+            <div class="offer-content-container col-lg-10 col-sm-12 col-12 d-flex flex-column flex-wrap align-items-center justify-content-center">
+                <h2>Make your occasion extra special with our deliciously customized cakes!</h2>
+				<a href="#"><button type="button" class=" btn btn-primary btn-lg col-lg-12 col-sm-12 col-12">ORDER NOW</button></a>
+            </div>
+        </div>
+    </div>
+    <div class="bg-image-container d-none d-xl-flex d-lg-flex d-md-flex align-item-center justify-self-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/custom-cakes.png" alt="Sliced Easter cake">
+    </div>
+</div>
+
 <?php get_footer();
