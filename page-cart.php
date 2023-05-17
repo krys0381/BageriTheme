51932fa46d2f5b2e03b7d280ce3619cff9164536
krys0381 <krys0381@easv365.dk>
2023-05-17 14:30:34 +0200

    Changing Cart and Checkout

--- page-cart.php
+++ page-cart.php
@@ -8,7 +8,7 @@
 	  	  	<div class="shop-sidebar-container d-flex flex-column col-xl-3 col-lg-3 col-sm-12 col-12">
                 <?php dynamic_sidebar( 'shop_sidebar_widget' ); ?>
 	  	  	</div>
-	  	  	<div class="shop-content-container d-flex flex-column align-items-center col-xl-9 col-lg-9 col-sm-12 col-12">
+	  	  	<div class="shop-content-container d-flex flex-row justify-content-center col-xl-9 col-lg-9 col-sm-12 col-12">
                 <?php if(have_posts()): ?>
 					<?php while(have_posts()): the_post() ?>
 						<?php the_content() ?>
